e897ba499e35b58d326b842ec13d662ff722756f
anyant <1075379406@qq.com>
2019-10-11 22:22:15 +0800

    Merge branch 'master' of https://github.com/ttanyan/dataView
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    demo

---
+++ Demo/demo01.py
@@ -0,0 +1,21 @@
+print("hello,world!")
+str="string"
+str1="str123"
+print(str+str1)
+import keyword
+print(keyword.kwlist)
+keyword.kwlist
+
+from sys import argv,path
+print('导入模块')
+print('path:',path)
+i=1
+if i>1 :
+   print('i是大于1的')
+elif i==1 :
+   print('i=1')
+
+else :
+   print('i<1')
+
+print(r'Ru\noob')
